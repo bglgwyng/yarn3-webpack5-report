49511e1a07740e2f102669948a26e7f1fff29c41
bglgwyng <bglgwyng@gmail.com>
2022-06-08 17:30:21 +0900

    Add guides.

--- packages/app/webpack.config.js
+++ packages/app/webpack.config.js
@@ -2,6 +2,7 @@ const { ProvidePlugin } = require('webpack');
 
 module.exports = {
   plugins: [
+    // NOTE: if you remove this, then build will succeed, but process won't be resolved properly.
     new ProvidePlugin({ process: 'process/browser' }),
   ],
 };
